1b1d6952233be563db0d38fed94847f82b9c1a3a
ivanshifman <ivanshifman1300@gmail.com>
2025-03-04 03:34:20 -0300

    config next change

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: { esmExternals: true },
   eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const ErrorTemplate = dynamic(() => import("../components/Error/ErrorTemplate"), {
-  ssr: false,
-});
+import ErrorTemplate from "../components/Error/ErrorTemplate";
 
 const NotFoundPage = () => {
   return (
