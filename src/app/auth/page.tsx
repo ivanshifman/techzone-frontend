060209e7f1d0f4c8055a99cc8c7c607da20cd8f0
ivanshifman <ivanshifman1300@gmail.com>
2025-03-04 01:48:01 -0300

    page auth suspense

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,11 @@
+import { Suspense } from "react";
 import { Col, Row } from "react-bootstrap";
+import Loading from "../../components/shared/Loading";
 import RegisterLogin from "../../components/Auth/RegisterLogin";
 
 const AuthPage = () => {
   return (
+    <Suspense fallback={<Loading />}>
     <Row>
       <Col sm={6} className="mt-3">
         <RegisterLogin />
@@ -11,6 +14,8 @@ const AuthPage = () => {
         <RegisterLogin isRegisterForm={true} />
       </Col>
     </Row>
+    </Suspense>
+    
   );
 };
 
