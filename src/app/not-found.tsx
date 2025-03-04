80cd0dc694753430ba69e77e8a2d6559198f907d
ivanshifman <ivanshifman1300@gmail.com>
2025-03-04 02:13:26 -0300

    delete suspense 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { Suspense } from "react";
 import ErrorTemplate from "../components/Error/ErrorTemplate";
-import Loading from "../components/shared/Loading";
 
 const NotFoundPage = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <ErrorTemplate
-        title="404 - Page Not Found"
-        message="The page you are looking for does not exist."
-      />
-    </Suspense>
+    <ErrorTemplate
+      title="404 - Page Not Found"
+      message="The page you are looking for does not exist."
+    />
   );
 };
 
