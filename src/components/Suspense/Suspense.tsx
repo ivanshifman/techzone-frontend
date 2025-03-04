2c7200364fba39eefdda2f414a384d8f52be8eb4
ivanshifman <ivanshifman1300@gmail.com>
2025-03-04 01:23:29 -0300

    more fix error suspense

--- src/components/Suspense/Suspense.tsx
+++ src/components/Suspense/Suspense.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import  { Suspense } from 'react';
-import AllProducts from '../../app/products/page';
-import Loading from '../shared/Loading';
-import TopHead from '../shared/TopHead';
-
+import { Suspense } from "react";
+import AllProducts from "../../app/products/page";
+import Loading from "../shared/Loading";
+import TopHead from "../shared/TopHead";
 
 const SuspenseWrapper = () => (
   <Suspense fallback={<Loading />}>
