ae9344fa7be6c98262eb25d6a615ca1ddc58685a
ivanshifman <ivanshifman1300@gmail.com>
2025-03-04 00:56:00 -0300

    suspense add

---
+++ src/components/Suspense/Suspense.tsx
@@ -0,0 +1,14 @@
+import  { Suspense } from 'react';
+import AllProducts from '../../app/products/page';
+import Loading from '../shared/Loading';
+import TopHead from '../shared/TopHead';
+
+
+const SuspenseWrapper = () => (
+  <Suspense fallback={<Loading />}>
+    <TopHead />
+    <AllProducts />
+  </Suspense>
+);
+
+export default SuspenseWrapper;
