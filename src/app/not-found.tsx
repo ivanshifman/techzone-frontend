d08e3e53a4626f3234a684abead5337b889e7384
ivanshifman <ivanshifman1300@gmail.com>
2025-03-04 12:25:31 -0300

    new fix error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { Suspense } from "react";
 import ErrorTemplate from "../components/Error/ErrorTemplate";
 
 export const dynamic = "force-dynamic";
 
 const NotFoundPage = () => {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <ErrorTemplate
       title="404 - Page Not Found"
       message="The page you are looking for does not exist."
     />
+    
+    </Suspense>
   );
 };
 
